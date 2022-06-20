46bcc99fe0570ee8e1094b26e6d962905e852b4f
Iqmaah <olabisiiqmah_2000@yahoo.com>
2022-06-20 16:44:39 +0100

    this

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <PaymentMethod/>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
