94ddb8516e8999f764e8c54bc4d440681a252e22
Iqmaah <olabisiiqmah_2000@yahoo.com>
2022-07-13 10:25:40 +0100

    edit

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Cards/>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
